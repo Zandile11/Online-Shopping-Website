2e6b9f7038ee8a0fb7d47712f9404c895388a690
zandile <zanon-g-025@student.wethinkcode.co.za>
2026-09-18 22:48:50 +0200

    Created and tested the TestCustomer

---
+++ src/main/java/TestCustomer.java
@@ -0,0 +1,6 @@
+public class TestCustomer {
+    public static void main(String[] args) {
+        Customer c = new Customer("mila@gmail.com", "panki123");
+        System.out.println(c.getEmail());
+    }
+}
